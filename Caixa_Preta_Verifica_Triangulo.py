286ab8ba05a498b5a16570a7039a7a6199613fc7
Leonardo <leonardoxabreu2009@hotmail.com>
2023-09-03 20:04:01 -0300

    Teste caixa preta test_tipo_triangulo - Leonardo Xavier

--- Caixa_Preta_Verifica_Triangulo.py
+++ Caixa_Preta_Verifica_Triangulo.py
@@ -19,6 +19,16 @@ class TestTrianguloFunctions(unittest.TestCase):
 
         # Teste com lados que possuem um valor igual a zero (não deve formar triângulo)
         self.assertFalse(eh_triangulo(0, 1, 2))
+    
+    def test_tipo_triangulo(self):
+        # Teste com triângulo equilátero
+        self.assertEqual(tipo_triangulo(3, 3, 3), "Equilátero")
+
+        # Teste com triângulo isósceles
+        self.assertEqual(tipo_triangulo(3, 3, 4), "Isósceles")
+
+        # Teste com triângulo escaleno
+        self.assertEqual(tipo_triangulo(3, 4, 5), "Escaleno")
 
 if __name__ == '__main__':
     unittest.main()
